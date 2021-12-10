378bd6cffa51b53f403551176070a89d09c82556
Thierry Oliveira <thierry_fernando@hotmail.com>
2021-12-10 18:28:24 -0400

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+gifts = [
+  ["Vinho Branco Censurado", 1, "gifts/vinho_branco.png"],
+  ["Vinho Tinto Censurado", 1, "gifts/vinho_tinto.png"],
+  ["Espumante Francês Veuve Noire Brut Rosé", 1, "gifts/veuve_noire.png"],
+  ["Necessaira Trousseau", 1, "gifts/necessaire.png"],
+  ["Livro Sapiens", 1, "gifts/livro_sapiens.png"],
+  ["Livro Rápido e Devagar", 1, "gifts/rapido_devagar.png"],
+  ["Livro Como fazer amigos e influenciar pessoas", 1, "gifts/amigos_influenciar.png"],
+  ["Livro Quem pensa enriquece", 1, "gifts/quem_pensa_enriquece.png"],
+  ["Livro Ganhar, gastar, investir: O Livro do Dinheiro para Mulheres", 1, "gifts/ganhar_gastar.png"]
+]
+
+gifts.each do |gift|
+  g = Gift.new(
+    name: gift[0],
+    quantity: gift[1],
+    photo: gift[2]
+  )
+  if g.save
+    puts "Gift #{g.name} criado com sucesso!"
+  end
+end
+puts "Criados #{Gift.count} gifts com sucesso."
